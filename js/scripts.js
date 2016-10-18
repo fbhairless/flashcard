760f319a95880955041d86aa7d4e97934ddf0c61
Hal Fairless and Nigel Burnett <hal@>
2016-10-18 13:56:33 -0700

    Add terms for definition, class and id tags for Javascript functions

---
+++ js/scripts.js
@@ -0,0 +1,39 @@
+$(document).ready(function() {
+
+  $(".click1").click(function() {
+    $("#JSinfo").fadeToggle();
+  });
+
+  $(".click2").click(function() {
+    $("#OpInfo").fadeToggle();
+  });
+
+  $(".click3").click(function() {
+    $("#Variables").fadeToggle();
+  });
+
+  $(".click4").click(function() {
+    $("#VariableNaming").fadeToggle();
+  });
+
+  $(".click5").click(function() {
+    $("#Functions").fadeToggle();
+  });
+
+  $(".click6").click(function() {
+    $("#Methods").fadeToggle();
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
